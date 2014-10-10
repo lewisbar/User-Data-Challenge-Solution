b8bce25d9c169006fe152bb82936cac7c2aeda10
Lennart Wisbar <Lennart@lennarts-mbp.fritz.box>
2014-10-10 14:00:48 +0200

    Initial Commit

---
+++ User Data Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Lennart Wisbar on 10.10.14.
+//  Copyright (c) 2014 Lennart Wisbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ User Data Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  User Data Challenge Solution
+//
+//  Created by Lennart Wisbar on 10.10.14.
+//  Copyright (c) 2014 Lennart Wisbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
